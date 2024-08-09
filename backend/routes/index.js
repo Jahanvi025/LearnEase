f7958d8159e990f2c71271a18d8042d6534a1c59
andhac <andhac84@yahoo.com>
2024-08-09 22:38:26 +0530

    Completed the courses route

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,7 +1,9 @@
 import express from "express";
 const router = express.Router();
 import auth from "./Auth/index.js";
+import course from "./course/index.js";
 
 router.use('/auth' , auth);
+router.use('/course' , course);
 
 export default router;
