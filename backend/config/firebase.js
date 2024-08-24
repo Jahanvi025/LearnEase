c2cef9285756552bf706ec902e4b9b2545350397
andhac <andhac84@yahoo.com>
2024-08-24 13:18:41 +0530

    Make the SignUp form workable

--- backend/config/firebase.js
+++ backend/config/firebase.js
@@ -1,5 +1,5 @@
 import admin from 'firebase-admin';
-import serviceAccout from './olms-9f450-firebase-adminsdk-b3gvs-092dc63578.json' assert {type: "json"};
+import serviceAccout from './olms-9f450-firebase-adminsdk-b3gvs-8b9640d480.json' assert {type: "json"};
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccout),
